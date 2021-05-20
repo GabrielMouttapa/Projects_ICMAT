f6a8bfb49f23af9a77bd71b9accde4a419bc98db
GabrielMouttapa <gabriel.mouttapa@gmail.com>
2021-05-20 14:28:31 +0200

    1st argument : file nae, 2nd argument : folder name, 3rd argument : searched word

--- Project1/script_var.sh
+++ Project1/script_var.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# 1st argument is the name of the file
-# 2nd argument is the folder in which we have to search the file
-# 3rd argument is the letter or the word to search in the file
-
 location=$(find ./${2} -name ${1});
 nb_lines=$(grep -c ${3} $location);
 nb_tot=$(grep -o ${3} $location | wc -l);
